55952969bf051b13fc52d699ec937032b6b8bb5b
MohammadABH <58365827+MohammadABH@users.noreply.github.com>
2022-03-06 14:11:16 +0000

    Argumentation algorithm (#4)
    * Fix read.me quotes
    
    * Add twitter API service
    
    * Add graph building logic
    
    * Add API layer
    
    * Add general metrics
    
    * Add argumentation algorithm and controller
    
    * Add error handling for keyword extraction
    
    * Update gitignore
    
    * Update gitignore for pycache
    
    * Remove pycache

--- backend/services/keyword_extraction_service.py
+++ backend/services/keyword_extraction_service.py
@@ -4,6 +4,7 @@ from yake import KeywordExtractor as Yake
 from transformers import AutoModel, AutoTokenizer
 from textblob import TextBlob
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
 from backend.services.utils.preprocessor_util import preprocess
 
 
@@ -51,10 +52,31 @@ class BertKeywordExtractor(KeywordExtractor):
     model = AutoModel.from_pretrained(MODEL, cache_dir=PATH)
     model.save_pretrained(PATH)  # Save model to specified path in disk to prevent re-downloading
 
-    def get_candidates(self, text):
+    def get_noun_candidates(self, text, n_grams=3):
         candidates = TextBlob(text).noun_phrases
-        candidates_three_grams = [sentence for sentence in candidates if len(sentence.split(' ')) < 3]
+        candidates_three_grams = [sentence for sentence in candidates if len(sentence.split(' ')) < n_grams]
         return candidates_three_grams
+
+    def get_n_grams_candidates(self, text, n_grams=3):
+        ngram_range = (1, n_grams)
+        stop_words = "english"
+
+        vectorizer = CountVectorizer(ngram_range=ngram_range, stop_words=stop_words)
+        count_matrix = vectorizer.fit([text])
+
+        candidates = count_matrix.get_feature_names_out().tolist()
+
+        return candidates
+
+    def get_candidates(self, text):
+        n_grams = 3
+        n_gram_candidates = self.get_n_grams_candidates(text, n_grams)
+        noun_candidates = self.get_noun_candidates(text, n_grams)
+
+        candidates = noun_candidates if len(noun_candidates) > 0 else n_gram_candidates
+
+        return candidates
+
 
     def get_embeddings_from_texts(self, text_list):
         tokens = BertKeywordExtractor.tokenizer(text_list, padding=True, return_tensors="pt")
@@ -73,6 +95,9 @@ class BertKeywordExtractor(KeywordExtractor):
     def get_keywords(self, text):
         pre_processed_text = preprocess(text)
         candidates = self.get_candidates(pre_processed_text)
+        if len(candidates) == 0:
+            # No possibly meaningful keyword can be extracted from the tweet, probably a tweet that doesn't make sense
+            return []
 
         candidate_embeddings = self.get_embeddings_from_texts(candidates)
         text_embedding = self.get_embeddings_from_texts([pre_processed_text])
@@ -93,4 +118,5 @@ class BertKeywordExtractor(KeywordExtractor):
 # print(yake_keyword_extractor.get_keywords(tweet))
 
 # keyword_extractor = BertKeywordExtractor()
+# print(keyword_extractor.get_top_keyword(tweet))
 # print(keyword_extractor.get_keywords(tweet))
--- backend/services/tweet_tree_builder_service.py
+++ backend/services/tweet_tree_builder_service.py
@@ -3,7 +3,7 @@ from itertools import chain
 from networkx.readwrite import json_graph
 import json
 from backend.services.twitter_api_service import TwitterAPIService
-from backend.services.keyword_extraction_service import YakeKeywordExtractor
+from backend.services.keyword_extraction_service import BertKeywordExtractor
 import matplotlib.pyplot as plt
 
 
@@ -23,6 +23,8 @@ class TweetTreeMetrics:
         self.root_neutral_sentiment_count = 0
         self.sentiment_towards_root = ""
 
+        self.max_public_metrics = 0
+
     def set_root_tweet_sentiment(self, root_tweet_sentiment):
         self.root_tweet_sentiment = root_tweet_sentiment
 
@@ -32,6 +34,13 @@ class TweetTreeMetrics:
     def set_strongest_argument_id(self, strongest_argument_id, argument_strength):
         if argument_strength > self.strongest_argument_id:
             self.strongest_argument_id = strongest_argument_id
+
+    def set_max_public_metrics(self, tweet):
+        public_metrics = tweet["public_metrics"]
+        score = public_metrics["like_count"] + public_metrics["retweet_count"]
+
+        if score > self.max_public_metrics:
+            self.max_public_metrics = score
 
     def increment_general_sentiment(self, sentiment_type):
         if sentiment_type == "positive":
@@ -53,7 +62,7 @@ class TweetTreeMetrics:
         values = [self.general_positive_sentiment_count,
                   self.general_negative_sentiment_count,
                   self.general_neutral_sentiment_count]
-        labels = ["Positive", "Negative", "Neutral"]
+        labels = ["positive", "negative", "neutral"]
 
         labels_with_values = zip(values, labels)
         max_label = max(labels_with_values)[1]  # Max label is the mode, as it is the max of all the sentiment counts
@@ -64,12 +73,15 @@ class TweetTreeMetrics:
         values = [self.root_positive_sentiment_count,
                   self.root_negative_sentiment_count,
                   self.root_neutral_sentiment_count]
-        labels = ["Positive", "Negative", "Neutral"]
+        labels = ["positive", "negative", "neutral"]
 
         labels_with_values = zip(values, labels)
         max_label = max(labels_with_values)[1]  # Max label is the mode, as it is the max of all the sentiment counts
 
         self.sentiment_towards_root = max_label
+
+    def get_max_public_metrics(self):
+        return self.max_public_metrics
 
     def get_metrics(self):
         return {
@@ -94,6 +106,7 @@ class TweetTree:
         root_id = root_tweet["id"]
         self.root = root_id
         self.tree.add_node(root_id, attributes=root_tweet)
+        self.metrics.set_max_public_metrics(root_tweet)
 
     def _parse_tweet(self, tweet):
         return {"id": tweet["id"],
@@ -109,9 +122,12 @@ class TweetTree:
             # If parent tweet is deleted, ignore tweet
             if tweet_parent_id in self.tree:
                 parsed_tweet = self._parse_tweet(tweet)
+                parsed_tweet["argumentative_type"] = "support"
                 self.tree.add_node(tweet_id, attributes=parsed_tweet)
                 self.tree.add_edge(tweet_parent_id, tweet_id, color="g", weight=3)
 
+                self.metrics.set_max_public_metrics(parsed_tweet)
+        
                 if tweet_parent_id == self.root:
                     self.metrics.increment_root_sentiment(parsed_tweet["sentiment"])
                 else:
@@ -142,7 +158,9 @@ class TweetTree:
 
 
 class TweetTreeBuilder:
-    keyword_extraction_service = YakeKeywordExtractor()
+  
+    keyword_extraction_service = BertKeywordExtractor()
+
 
     def __init__(self, tweet_id):
         self.twitter_api_service = TwitterAPIService(
@@ -152,6 +170,7 @@ class TweetTreeBuilder:
     def _build_tweet_tree(self, tweet_id):
         # Build tweet tree
         tweet = self.twitter_api_service.get_tweet(tweet_id)
+        tweet["argumentative_type"] = "none"
         tweet_text = tweet["text"]
         tweet_conversation_thread = self.twitter_api_service.get_conversation_thread(tweet_id)
 
@@ -163,7 +182,9 @@ class TweetTreeBuilder:
 
         # Build related tweet trees and append them to main tweet tree
         related_tweets = self._get_related_tweets(tweet_text)
+        # related_tweets = []
         for related_tweet in related_tweets:
+            related_tweet["argumentative_type"] = "support"
             # Only retrieve 'fresh' tweets that are not replies or are retweets tweets
             if 'referenced_tweets' not in related_tweet and related_tweet['id'] != tweet['id']:
                 related_tweet_thread = self.twitter_api_service.get_conversation_thread(related_tweet['id'])
